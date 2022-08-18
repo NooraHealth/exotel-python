b645d66e8ad152cf88394bb53a9b20d4b5a03893
Udit <23015406+udit-001@users.noreply.github.com>
2022-08-18 13:39:42 +0530

    Refactored how auth_headers are passed

--- pyexotel/pyexotel.py
+++ pyexotel/pyexotel.py
@@ -10,14 +10,15 @@ class Exotel:
         self.key = key
         self.token = token
         self.baseurl = urljoin(baseurl, "v2/accounts/{sid}/".format(sid=sid))
+        self.auth_headers = HTTPBasicAuth(self.key, self.token)
 
     def get_campaign_details(self, campaign_id):
-        return requests.get(urljoin(self.baseurl, 'campaigns/{cid}'.format(cid=campaign_id)), 
-        auth=HTTPBasicAuth(self.key, self.token))
+        return self._sesh.get(urljoin(self.baseurl, 'campaigns/{cid}'.format(cid=campaign_id)), 
+        auth=self.auth_headers)
 
     def get_campaign_call_details(self, campaign_id):
         return requests.get(urljoin(self.baseurl, 'campaigns/{cid}/call-details'.format(cid=campaign_id)),
-        auth=HTTPBasicAuth(self.key, self.token))
+        auth=self.auth_headers)
 
     def get_bulk_campaign_details(self):
-        return requests.get(urljoin(self.baseurl, 'campaigns'), auth=HTTPBasicAuth(self.key, self.token))
+        return requests.get(urljoin(self.baseurl, 'campaigns'), auth=self.auth_headers)
